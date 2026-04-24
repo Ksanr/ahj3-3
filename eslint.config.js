0c6eb8e9e5951bb36054a5874ab2f1c2ae35590e
Aleksandr Lipunov <alipunov@mail.ru>
2026-04-24 17:08:05 +0000

    fix Lint

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,6 @@ module.exports = [
             sourceType: "module",
             globals: {
                 ...globals.browser,
-                ...globals.node,
                 ...globals.jest,
             },
         },
@@ -23,11 +22,28 @@ module.exports = [
         },
     },
     {
+        files: ["*.config.js", "*.setup.js", "eslint.config.js"],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+            },
+        },
+        rules: {
+            "no-console": "off",
+            "no-unused-vars": "warn",
+            "no-undef": "off",
+        },
+    },
+    {
         ignores: [
             "dist/",
             "node_modules/",
-            "webpack.config.js",
             "coverage/",
+            ".yarn/",
+            "**/*.test.js",
+            "webpack.config.js",
         ],
     },
 ];
